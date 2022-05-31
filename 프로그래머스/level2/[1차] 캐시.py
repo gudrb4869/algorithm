55bbe0015780dbc3472ccd26bdcd95a62a2c1a89
HyungKyuPark <gudrb4869@naver.com>
2022-06-01 00:06:31 +0900

    2022년 6월 1일 풀이완료

---
+++ 프로그래머스/level2/[1차] 캐시.py
@@ -0,0 +1,16 @@
+from collections import deque
+
+def solution(cacheSize, cities):
+    answer = 0
+    cache = deque(maxlen=cacheSize)
+
+    for city in cities:
+        city = city.lower()
+        if city in cache:
+            answer += 1
+            cache.remove(city)
+        else:
+            answer += 5
+        cache.append(city)
+    
+    return answer
