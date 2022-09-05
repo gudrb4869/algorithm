928b642d5354bc5e057c9e11d5add76826b73f0a
HyungKyuPark <gudrb4869@naver.com>
2022-09-05 19:27:59 +0900

    2022-09-05 2022 KAKAO TECH INTERNSHIP

---
+++ 프로그래머스/level3/코딩 테스트 공부.py
@@ -0,0 +1,17 @@
+def solution(alp, cop, problems):
+    max_alp, max_cop = 0, 0
+    for a, c, _, _, _ in problems:
+        max_alp, max_cop = max(max_alp, a), max(max_cop, c)
+    alp, cop = min(alp, max_alp), min(cop, max_cop)
+    INF = float('inf')
+    dp = [[INF] * (max_cop + 1) for _ in range(max_alp + 1)]
+    dp[alp][cop] = 0
+    for i in range(alp, max_alp + 1):
+        for j in range(cop, max_cop + 1):
+            dp[i][j] = min(dp[i][j], dp[i - 1][j] + 1 if i - 1 >= alp else INF, dp[i][j - 1] + 1 if j - 1 >= cop else INF)
+    
+            for alp_req, cop_req, alp_rwd, cop_rwd, cost in problems:
+                if i >= alp_req and j >= cop_req:
+                    next_alp, next_cop = min(max_alp, i + alp_rwd), min(max_cop, j + cop_rwd)
+                    dp[next_alp][next_cop] = min(dp[next_alp][next_cop], dp[i][j] + cost)
+    return dp[-1][-1]
